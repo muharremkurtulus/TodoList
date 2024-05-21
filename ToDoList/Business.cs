f81c75eafbfc22844bc59e7943767bdcad2ad381
Muharrem Kurtuluş <kurtulusmuharrem@outlook.com>
2024-05-22 00:32:03 +0300

    Todo List
    Todo List Project

---
+++ ToDoList/Business.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using ToDoList;
+
+public class TodoList
+{
+    private List<TodoItems> items = new List<TodoItems>();
+    private int nextId = 1;
+
+    public void AddItem(string description)
+    {
+        var item = new TodoItems { Id = nextId++, Description = description, IsCompleted = false };
+        items.Add(item);
+    }
+
+    public void CompleteItem(int id)
+    {
+        var item = items.Find(i => i.Id == id);
+        if (item != null)
+        {
+            item.IsCompleted = true;
+        }
+    }
+
+    public void RemoveItem(int id)
+    {
+        var item = items.Find(i => i.Id == id);
+        if (item != null)
+        {
+            items.Remove(item);
+        }
+    }
+
+    public List<TodoItems> GetItems()
+    {
+        return items;
+    }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var todoList = new TodoList();
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("To-Do List:");
+            foreach (var item in todoList.GetItems())
+            {
+                Console.WriteLine($"{item.Id}. {item.Description} - {(item.IsCompleted ? "Completed" : "Pending")}");
+            }
+
+            Console.WriteLine("\nOptions:");
+            Console.WriteLine("1. Add a new item");
+            Console.WriteLine("2. Complete an item");
+            Console.WriteLine("3. Remove an item");
+            Console.WriteLine("4. Exit");
+
+            var input = Console.ReadLine();
+            if (input == "4") break;
+
+            switch (input)
+            {
+                case "1":
+                    Console.Write("Enter the description of the new item: ");
+                    var description = Console.ReadLine();
+                    todoList.AddItem(description);
+                    break;
+                case "2":
+                    Console.Write("Enter the ID of the item to complete: ");
+                    if (int.TryParse(Console.ReadLine(), out int completeId))
+                    {
+                        todoList.CompleteItem(completeId);
+                    }
+                    break;
+                case "3":
+                    Console.Write("Enter the ID of the item to remove: ");
+                    if (int.TryParse(Console.ReadLine(), out int removeId))
+                    {
+                        todoList.RemoveItem(removeId);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+        }
+    }
+}
